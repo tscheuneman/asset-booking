eb7f130477c45a89610a8e552125dfab352fac73
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-24 10:44:24 -0700

    Configure Image Resizer to accept variable data values

--- app/Jobs/ProcessImage.php
+++ app/Jobs/ProcessImage.php
@@ -18,15 +18,17 @@ class ProcessImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $fileLoc;
+    protected $fileLoc, $width, $quality;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($fileLocation)
+    public function __construct($fileLocation, $width, $quality = 60)
     {
         $this->fileLoc = $fileLocation;
+        $this->width = $width;
+        $this->quality = $quality;
     }
 
     /**
@@ -38,13 +40,14 @@ class ProcessImage implements ShouldQueue
     {
         $manager = new ImageManager(array('driver' => 'imagick'));
         try {
-            $img = $manager->make(public_path(). '/storage/' . $this->fileLoc)->resize(500, null, function ($constraint) {
+            $img = $manager->make(public_path(). '/storage/' . $this->fileLoc)->resize($this->width, null, function ($constraint) {
                 $constraint->aspectRatio();
             });
-            $img->save(public_path(). '/storage/' . $this->fileLoc, 60);
+            $img->save(public_path(). '/storage/' . $this->fileLoc, $this->quality);
         }
         catch (Exception $e) {
             report($e);
+
          }
 
 
