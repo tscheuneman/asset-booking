7858197b265681c247b478470f943200343e88fd
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-24 10:01:49 -0700

    Create job for resizing markers

---
+++ app/Jobs/ProcessMarker.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ProcessMarker implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($fileLocation)
+    {
+        $this->markerLoc = $fileLocation;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $manager = new ImageManager(array('driver' => 'imagick'));
+        try {
+            $img = $manager->make(public_path(). '/storage/' . $this->fileLoc)->resize(14, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $img->save(public_path(). '/storage/' . $this->fileLoc, 60);
+        }
+        catch (Exception $e) {
+            report($e);
+        }
+    }
+}
