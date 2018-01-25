63e98e1c187ad2bccdc2bb452c5d168d33f136b5
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-24 17:06:59 -0700

    Create controller to get our booking data

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Booking;
 
 class BookingController extends Controller
 {
     public function bookings(Request $request)
     {
+
         $this->validate(request(), [
-            'id' => 'exists:assets',
+            'asset_id' => 'required|integer|exists:assets,id',
         ]);
-        return request('asset_id');
+
+        $asset_id = request('asset_id');
+        $date = date("Y-m-d H:i:s", strtotime('+3 months'));
+
+        return Booking::where('asset_id', '=', $asset_id)->where('time_to', '<=', $date)->get();
 
     }
 }
