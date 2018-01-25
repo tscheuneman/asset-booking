c2260e3b7dc617906f717aba3073c09d645e9a54
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-25 11:57:46 -0700

    import laraveluuid

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Xavrsl\Cas\CasServiceProvider::class,
+        Emadadly\LaravelUuid\LaravelUuidServiceProvider::class,
 
     ],
 
