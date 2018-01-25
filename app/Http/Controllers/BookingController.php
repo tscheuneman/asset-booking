d7faa63dd684705fab10bd5c162845cf1cb2933d
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-25 15:54:30 -0700

    Create logic to create booking event

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Booking;
+use Validator;
+use Cas;
+use Illuminate\Support\Facades\Auth;
+
 
 class BookingController extends Controller
 {
@@ -11,7 +15,7 @@ class BookingController extends Controller
     {
 
         $this->validate(request(), [
-            'asset_id' => 'required|integer|exists:assets,id',
+            'asset_id' => 'required|string|exists:assets,id',
         ]);
 
         $asset_id = request('asset_id');
@@ -20,4 +24,64 @@ class BookingController extends Controller
         return Booking::where('asset_id', '=', $asset_id)->where('time_to', '<=', $date)->get();
 
     }
+
+    public function store(Request $request, $id)
+    {
+        $returnData = array();
+        $validator = Validator::make($request->all(), [
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            $returnData['status'] = 'Error';
+            $returnData['message'] = 'Invalid Date';
+            return json_encode($returnData);
+        }
+
+
+
+        $startDate = request('start_date');
+        $endDate = request('end_date');
+
+        if($startDate == date('Y-m-d') && $endDate == date('Y-m-d')) {
+            $returnData['status'] = 'Error';
+            $returnData['message'] = 'That is an invalid date!';
+            return json_encode($returnData);
+        }
+
+        $val = Booking::where('asset_id', '=', $id)->where('time_from', '>=', $startDate)->where('time_to', '<=', $endDate)->first();
+        if($val === null) {
+            if (Auth::check())
+            {
+
+                $user = Auth::id();
+                $asset_id = $id;
+
+                $startTime = date('Y-m-d H:i:s', strtotime($startDate));
+                $endTime = date('Y-m-d H:i:s', strtotime($endDate));
+
+                $booking = new Booking();
+                    $booking->asset_id = $asset_id;
+                    $booking->time_from = $startTime;
+                    $booking->time_to = $endTime;
+                    $booking->cust_id = $user;
+                    $booking->save();
+
+                $returnData['status'] = 'Success';
+                $returnData['message'] = 'That time has been booked!';
+                return json_encode($returnData);
+            }
+            else {
+                return "Error, not logged in";
+            }
+        }
+        else {
+            $returnData['status'] = 'Error';
+            $returnData['message'] = 'That time range is already booked!';
+            return json_encode($returnData);
+        }
+
+    }
+
 }
