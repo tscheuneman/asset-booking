f320bbf53627c6e653feff2f99da4dae54f810a9
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-30 09:44:21 -0700

    Return more search results

--- app/Building.php
+++ app/Building.php
@@ -13,7 +13,7 @@ class Building extends Model
 
     public static function getByDistance($lat, $lng, $distance)
     {
-        $results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians(latitude) ) ) ) AS distance FROM buildings HAVING distance < ' . $distance . ' ORDER BY distance LIMIT 12'));
+        $results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians(latitude) ) ) ) AS distance FROM buildings HAVING distance < ' . $distance . ' ORDER BY distance LIMIT 18'));
         return (array)$results;
     }
 
