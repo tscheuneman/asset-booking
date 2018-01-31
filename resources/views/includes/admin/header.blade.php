3593c584c684fa5d4f5f71e00a1f3664af99db33
Thomas Scheuneman <thomas@tswebvisions.com>
2018-01-31 12:48:13 -0700

    Adjust admin/installer header
    Changed content
    
    Updated styles

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -1,3 +1,3 @@
  <div class="container">
-     <h1>{{env('APP_NAME', 'Asset Booking App')}} | <span class="headerTitDesc"> Admin </span></h1>
+     <h1>{{env('APP_NAME', 'Asset Booking App')}} <span class="headerTitDesc"> Admin </span></h1>
  </div>
