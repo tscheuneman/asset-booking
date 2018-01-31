b413a600de9bf351b2f266f1b55620083271e28e
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2018-01-30 17:36:27 -0700

    Create InstallerController, define logic
    Create logic for getting main page.  Returning creation page.  Also creating an installer

--- app/Http/Controllers/InstallerController.php
+++ app/Http/Controllers/InstallerController.php
@@ -4,13 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Cas;
+use App\Installer;
+use Illuminate\Support\Facades\Hash;
+
 class InstallerController extends Controller
 {
     public function index() {
-        return view('installer.main',
+        $installer = Installer::paginate(25);
+        return view('admin.installers',
             [
-                'user' => Cas::user(),
+                'installers' => $installer
             ]
         );
+    }
+
+    public function create() {
+        return view('admin.installerCreate');
+    }
+
+    public function store() {
+        $installer = new Installer();
+
+        $this->validate(request(), [
+            'username' => 'required|unique:installers',
+            'email' => 'required|email',
+            'company' => 'required',
+        ]);
+
+        $installer->username = request('username');
+        $installer->email = request('email');
+        $installer->company = request('company');
+        $installer->password = Hash::make(request('username'));
+
+        $installer->save();
+
+        \Session::flash('flash_created', request('username') . ' has been created');
+        return redirect('/admin/installers');
     }
 }
