755dc4fab3f4a659e394840e347922db20654bf9
Thomas Scheuneman <thomas@tswebvisions.com>
2018-02-07 16:42:04 -0700

    Create headings for installer section

--- resources/views/installers/main.blade.php
+++ resources/views/installers/main.blade.php
@@ -3,5 +3,46 @@
     Installers
 @stop
 @section('content')
+    <br>
+    @if(Session::has('flash_deleted'))
+        <div class="alert alert-warning"><span class="glyphicon glyphicon-remove-circle"></span><em> {!! session('flash_deleted') !!}</em></div>
+    @endif
+    @if(Session::has('flash_created'))
+        <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_created') !!}</em></div>
+    @endif
+    <br>
+    <div class="row">
+        <div class="col">
+            <div class="adminHeading">
+                <section>
+                    <div class="icon glyphicon glyphicon-hourglass blue"></div>
+                    <div class="info">
+                        <span class="data">
+                            {{$pendingBookings}}
+                        </span>
+                        <span class="desc">
+                            Pending
+                        </span>
+                    </div>
+                </section>
+            </div>
 
+            <div class="col">
+                <div class="adminHeading">
+                    <section>
+                        <div class="icon glyphicon glyphicon-ban-circle red"></div>
+                        <div class="info">
+                            <span class="data">
+                                {{$lateBookings}}
+                            </span>
+                            <span class="desc">
+                              Late
+                            </span>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+
+        </div>
 @stop
