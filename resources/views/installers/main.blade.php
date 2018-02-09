b7fb40cc5f7453b7b05aa1e3a3e103cd02509651
Thomas Scheuneman <thomas@tswebvisions.com>
2018-02-09 14:19:51 -0700

    Create installer booking event table

--- resources/views/installers/main.blade.php
+++ resources/views/installers/main.blade.php
@@ -42,7 +42,48 @@
                     </section>
                 </div>
             </div>
-
-
         </div>
+        <br class="clear" />
+        <hr>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Asset Name</th>
+                <th scope="col">Region</th>
+                <th scope="col">Building</th>
+                <th scope="col">Customer</th>
+                <th scope="col">Date Range</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($bookingInfo as $book)
+                    @if(date('Y-m-d', strtotime($book->time_from)) <= date('Y-m-d'))
+                        <tr class="past_due">
+                    @else
+                        <tr>
+                    @endif
+                        <td>
+                            {{$book->asset->name}}
+                        </td>
+                        <td>
+                            {{$book->asset->location->region->name}}
+                        </td>
+                        <td>
+                            {{$book->asset->location->building->name}}
+                        </td>
+                        <td>
+                            {{$book->customer->email}}
+                        </td>
+                        <td>
+                            {{date('Y-m-d', strtotime($book->time_from))}} - {{date('Y-m-d', strtotime($book->time_to))}}
+                        </td>
+                        <td>
+                            <a class="editAction" href="/installers/install/{{$book->id}}"><span class="glyphicon glyphicon-wrench"> </span> Install</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 @stop
