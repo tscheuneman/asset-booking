749d690c835ecae07366b5e87a3f799cb351b647
Thomas Scheuneman <thomas@tswebvisions.com>
2018-02-16 15:35:15 -0700

    Add Installer Controller

---
+++ app/Http/Controllers/Installers/InstallController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Installers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Booking;
+use Cas;
+
+class InstallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $booking = Booking::with('asset', 'asset.location.building', 'asset.location.region', 'customer')->find($id);
+        $installer = Cas::user();
+        return view('installers.bookings.bookings',
+            [
+                'booking' => $booking,
+                'installer' => $installer
+            ]
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
