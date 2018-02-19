bdd59d153c3a9ea1adb06a7ce3ae95728b598861
Thomas Scheuneman <thomas@tswebvisions.com>
2018-02-19 11:19:59 -0700

    Create an installation model

---
+++ app/Installation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Emadadly\LaravelUuid\Uuids;
+
+
+class Installation extends Model
+{
+    use Uuids;
+    public $incrementing = false;
+
+    //
+}
