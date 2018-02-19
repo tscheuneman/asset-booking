6f97896c2d5f7ba16290b7d1aa3722decff6530d
Thomas Scheuneman <thomas@tswebvisions.com>
2018-02-19 11:20:18 -0700

    Create storage method for InstallController

--- app/Http/Controllers/Installers/InstallController.php
+++ app/Http/Controllers/Installers/InstallController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Installers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Booking;
+use App\Asset;
 use Cas;
+use File;
+use App\Jobs\ProcessImage;
+use App\Installation;
 
 class InstallController extends Controller
 {
@@ -35,9 +39,52 @@ class InstallController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        $this->validate(request(), [
+            'book_id' => 'required|string|exists:bookings,id',
+            'asset_id' => 'string|required|exists:assets,id',
+            'cust_id' => 'string|required|exists:users,id',
+            'installer' => 'required|string|exists:installers,username',
+            'notes' => 'nullable|string',
+            'image' => 'required|image',
+        ]);
+
+        try {
+            $path = $request->file('image')->store(
+                'images/', 'public'
+            );
+        }
+        catch(Exception $e) {
+            \Session::flash('flash_deleted',request('name') . ' Error uploading file');
+            return redirect('/admin/assets');
+        }
+
+        try {
+
+            $asset = Asset::find(request('asset_id'));
+            File::delete(public_path() . '/storage/' . $asset->latest_image);
+            $asset->latest_image = $path;
+            ProcessImage::dispatch($path, 500, 60);
+
+            $asset->save();
+
+            $booking = Booking::find($id);
+                $booking->active = false;
+                $booking->save();
+
+             $installation = new Installation();
+                $installation->customer_id = request('cust_id');
+                $installation->asset_id = request('asset_id');
+                $installation->booking_id = request('book_id');
+                $installation->save();
+
+            \Session::flash('flash_created', 'Asset has been installed');
+            return redirect('/installers');
+        } catch(QueryException $e) {
+            \Session::flash('flash_deleted','Error Installing');
+            return redirect('/installers/install/' . $id);
+        }
     }
 
     /**
