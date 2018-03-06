9b10c9bee32c1fa8b20d7b30b232abe5bb4b2e29
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-06 11:57:55 -0700

    Cart Entry Component

---
+++ resources/assets/js/components/CartEntry.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="entry">
+        <span class="asset">
+            <strong>{{booking.asset.name}}</strong>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            booking: Object
+        },
+        mounted(){
+
+        },
+        name: "cart-entry"
+    }
+</script>
+
+<style scoped>
+
+</style>
