a185e3c201ea399daa5204744aec7979f0666ddc
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-06 11:59:51 -0700

    Store changes, locat booking events and user info

--- resources/assets/js/components/store.js
+++ resources/assets/js/components/store.js
@@ -4,19 +4,36 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        count: 0
+        bookingEvents: [],
+        count: 0,
+        username: '',
+        first_name: '',
+        last_name: '',
     },
     getters: {
         countGet: state => state.count,
+        getFirstName: state => state.first_name,
+        getCartItems: state => {
+            return state.bookingEvents;
+        }
     },
     mutations: {
         change (state, value) {
             // mutate state
             state.count = value;
         },
+        changeFirstName (state, value) {
+            state.first_name = value;
+        },
+        addBookingEvent (state, value) {
+            state.bookingEvents = value;
+        },
         increment (state) {
             // mutate state
             state.count++;
         },
+        addToCart (state, value) {
+            state.bookingEvents.push(value);
+        }
     }
 });
