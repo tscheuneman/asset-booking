1aaf1d08b1e4d0e1a77fcbfccb6d3491033337d1
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-08 11:12:59 -0700

    Store query as seperate string

--- app/Building.php
+++ app/Building.php
@@ -13,7 +13,8 @@ class Building extends Model
 
     public static function getByDistance($lat, $lng, $distance)
     {
-        $results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians(latitude) ) ) ) AS distance FROM buildings HAVING distance < ' . $distance . ' ORDER BY distance LIMIT 18'));
+        $query = 'SELECT id, ( 3959 * acos( cos( radians('.$lat.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('. $lng .') ) + sin( radians('.$lat.') ) * sin( radians(latitude) ) ) ) AS distance FROM buildings HAVING distance < ' .$distance. ' ORDER BY distance LIMIT 18';
+        $results = DB::select(DB::raw($query));
         return (array)$results;
     }
 
