94058adf08869cc18268866d79b99a57505f856e
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-08 15:02:20 -0700

    show cart entries

--- resources/assets/js/components/CartEntry.vue
+++ resources/assets/js/components/CartEntry.vue
@@ -1,12 +1,18 @@
+
 <template>
     <div class="entry">
         <span class="asset">
-            <strong>{{booking.asset.name}}</strong>
+            <span class="assetTitle">{{booking.m.booking.asset.name}}</span>
+            <span><strong>Building:</strong> {{booking.m.booking.asset.location.building.name}}</span>
+            <span><strong>Region:</strong> {{booking.m.booking.asset.location.region.name}}</span>
+            <span><strong>From:</strong> {{booking.m.booking.time_from | dateFormat}} - <strong>To: </strong> {{booking.m.booking.time_to | dateFormat}}</span>
         </span>
     </div>
 </template>
 
 <script>
+    import moment from 'moment'
+
     export default {
         props: {
             booking: Object
@@ -14,10 +20,29 @@
         mounted(){
 
         },
+        filters: {
+            dateFormat: function (value) {
+                if (value) {
+                    return moment(String(value)).format('MMMM Do YYYY')
+                }
+            }
+        },
         name: "cart-entry"
     }
 </script>
 
 <style scoped>
-
+    div.entry {
+        padding: 15px;
+        border: 1px solid #ccc;
+        margin: 18px 0;
+        border-radius: 13px;
+    }
+    span.assetTitle {
+        font-size:19px;
+        font-weight:bold;
+    }
+    span {
+        display:block;
+    }
 </style>
