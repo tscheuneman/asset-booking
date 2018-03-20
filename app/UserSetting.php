503682bdd3a48a49e5d33b688328237ebb2226db
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-20 15:57:35 -0700

    Ensure settings uses uuid

--- app/UserSetting.php
+++ app/UserSetting.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Emadadly\LaravelUuid\Uuids;
 
 class UserSetting extends Model
 {
-    //
+    use Uuids;
+    public $incrementing = false;
+
 }
