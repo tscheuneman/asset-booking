9e649c7e93f42b264da98b017e9534d4e398e9e5
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-20 15:58:34 -0700

    Create seeder

--- database/seeds/GlobalSettingsSeeder.php
+++ database/seeds/GlobalSettingsSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Setting;
+Use App\AdminSetting;
 
 class GlobalSettingsSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class GlobalSettingsSeeder extends Seeder
      */
     public function run()
     {
+        $this->createSetting('Enable Cas', 'enable-cas', 'checkbox', 'Enable CAS for site?', true, false);
+    }
 
+    public static function createSetting($name, $slug, $type, $desc, $global, $val) {
+        $setting = new Setting();
+            $setting->name = $name;
+            $setting->slug = $slug;
+            $setting->type = $type;
+            $setting->description = $desc;
+            $setting->global = $global;
+            $setting->admin = true;
+        $setting->save();
+
+
+        $adminSetting = new AdminSetting();
+            $adminSetting->setting_id = $setting->id;
+            $adminSetting->admin_id = '00000000-0000-0000-0000-000000000000';
+            $adminSetting->value = $val;
+        $adminSetting->save();
     }
 }
