25e939f57d8bcdd3b37214a99a0a3cabe04a18d8
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-20 11:58:38 -0700

    Add orderable col in overall cat view

--- resources/views/admin/categories/category.blade.php
+++ resources/views/admin/categories/category.blade.php
@@ -17,34 +17,41 @@
         <tr>
             <th scope="col" colspan="2">Name</th>
             <th scope="col">Slug</th>
+            <th scope="col">Orderable</th>
             <th scope="col">Updated</th>
             <th scope="col">Actions</th>
         </tr>
         </thead>
         <tbody>
-
-        @foreach($categories as $category)
-            <tr>
-            <td colspan="2">
-              {{$category->name}}
-            </td>
-            <td>
-             {{$category->slug}}
-            </td>
-            <td>
-                {{$category->updated_at->format('Y-m-d')}}
-            </td>
-            <td>
-                <a class="editAction" href="/admin/category/edit/{{$category->id}}"><span class="glyphicon glyphicon-pencil"> </span> Edit</a>
-            </td>
-            <tr>
-                @foreach($category->subcats as $subCat)
-                  @include('layouts.categories.categoryLooper', array(
-                  'subCat' => $subCat,
-                  'offset' => 0
-                  ))
-                @endforeach
-        @endforeach
+            @foreach($categories as $category)
+                <tr>
+                <td colspan="2">
+                  {{$category->name}}
+                </td>
+                <td>
+                 {{$category->slug}}
+                </td>
+                <td>
+                    @if($category->orderable)
+                        Yes
+                    @else
+                        No
+                    @endif
+                </td>
+                <td>
+                    {{$category->updated_at->format('Y-m-d')}}
+                </td>
+                <td>
+                    <a class="editAction" href="/admin/category/edit/{{$category->id}}"><span class="glyphicon glyphicon-pencil"> </span> Edit</a>
+                </td>
+                <tr>
+                    @foreach($category->subcats as $subCat)
+                      @include('layouts.categories.categoryLooper', array(
+                      'subCat' => $subCat,
+                      'offset' => 0
+                      ))
+                    @endforeach
+            @endforeach
         </tbody>
     </table>
 
