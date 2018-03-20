156f0ea814dce3c2631b7da9e97178c26ee53190
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-20 15:56:56 -0700

    Admin settings migration

---
+++ database/migrations/2018_03_20_220414_create_admin_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_settings', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->primary('id');
+            $table->uuid('setting_id')->nullable(false);
+            $table->uuid('admin_id')->nullable(false);
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_settings');
+    }
+}
