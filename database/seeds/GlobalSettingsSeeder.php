547f5c8c2eb90b2d34005aaf418475354db46cb5
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-21 14:06:28 -0700

    Add settings to seeder

--- database/seeds/GlobalSettingsSeeder.php
+++ database/seeds/GlobalSettingsSeeder.php
@@ -13,11 +13,18 @@ class GlobalSettingsSeeder extends Seeder
      */
     public function run()
     {
+        Setting::truncate();
+        AdminSetting::truncate();
 
         $this->createSetting('Site Name', 'site-name', 'textbox', 'Name of site', true, "Asset Booking");
         $this->createSetting('Entries per Page', 'entries-per-page', 'number', 'Number of entries per page', true, 10);
-        $this->createSetting('Enable Cas', 'enable-cas', 'checkbox', 'Enable CAS for site?', true, true);
 
+        $this->createSetting('Default Email', 'default-email', 'email', 'Default email address to send entries too', true, 'example@example.com');
+
+        $this->createSetting('Enable Cas', 'enable-cas', 'checkbox', 'Enable CAS for site?', true, true);
+        $this->createSetting('Multi Level Categories', 'multi-level-cats', 'checkbox', 'Enable multi level categories', true, true);
+        $this->createSetting('Orderable Categories', 'orderable-categories', 'checkbox', 'Enable orderable products', true, true);
+        $this->createSetting('User Approval', 'user-approval', 'checkbox', 'Require user approval', true, true);
     }
 
     public static function createSetting($name, $slug, $type, $desc, $global, $defaultValue) {
