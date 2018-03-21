10b8b2117f98b06ca1166ce4efa1c330d17e771e
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-21 12:39:00 -0700

    Create service provider for admin settings

---
+++ app/Providers/AdminSettingsServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Setting;
+use Illuminate\Contracts\Cache\Factory;
+
+class AdminSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+
+    public function boot(Factory $cache)
+    {
+        $globalSettings = Setting::with('adminSetting')->where('global', '=', true)->get();
+        $globalSettings = $cache->remember('settings', 60, function() use ($globalSettings)
+        {
+            // Laravel >= 5.2, use 'lists' instead of 'pluck' for Laravel <= 5.1
+            return $globalSettings->pluck('adminSetting.value', 'slug')->all();
+        });
+
+        $adminSettings = Setting::with('adminSetting')->where('global', '=', false)->get();
+        $adminSettings = $cache->remember('settings', 60, function() use ($adminSettings)
+        {
+            // Laravel >= 5.2, use 'lists' instead of 'pluck' for Laravel <= 5.1
+            return $adminSettings->pluck('adminSetting.value', 'slug')->all();
+        });
+
+        config()->set('globalSettings', $globalSettings);
+        config()->set('adminSettings', $adminSettings);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
