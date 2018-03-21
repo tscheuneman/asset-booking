e513cffc330b3a28c885bab94166077a86308f3a
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-21 12:39:19 -0700

    See our required global settings

--- database/seeds/GlobalSettingsSeeder.php
+++ database/seeds/GlobalSettingsSeeder.php
@@ -13,24 +13,32 @@ class GlobalSettingsSeeder extends Seeder
      */
     public function run()
     {
-        $this->createSetting('Enable Cas', 'enable-cas', 'checkbox', 'Enable CAS for site?', true, false);
+
+        $this->createSetting('Site Name', 'site-name', 'textbox', 'Name of site', true, "Asset Booking");
+        $this->createSetting('Entries per Page', 'entries-per-page', 'number', 'Number of entries per page', true, 10);
+        $this->createSetting('Enable Cas', 'enable-cas', 'checkbox', 'Enable CAS for site?', true, true);
+
     }
 
-    public static function createSetting($name, $slug, $type, $desc, $global, $val) {
-        $setting = new Setting();
+    public static function createSetting($name, $slug, $type, $desc, $global, $defaultValue) {
+        $setting = Setting::where('slug', '=', $slug)->first();
+        if($setting == null) {
+            $setting = new Setting();
             $setting->name = $name;
             $setting->slug = $slug;
             $setting->type = $type;
             $setting->description = $desc;
             $setting->global = $global;
             $setting->admin = true;
-        $setting->save();
+            $setting->save();
 
 
-        $adminSetting = new AdminSetting();
+            $adminSetting = new AdminSetting();
             $adminSetting->setting_id = $setting->id;
             $adminSetting->admin_id = '00000000-0000-0000-0000-000000000000';
-            $adminSetting->value = $val;
-        $adminSetting->save();
+            $adminSetting->value = $defaultValue;
+            $adminSetting->save();
+        }
+
     }
 }
