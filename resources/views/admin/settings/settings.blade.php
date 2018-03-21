8a6b3bc54f097cda70585584ef4e8a0b4c0c45a8
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-21 14:07:21 -0700

    Layout for displaying settings

--- resources/views/admin/settings/settings.blade.php
+++ resources/views/admin/settings/settings.blade.php
@@ -3,6 +3,27 @@
     Settings
 @stop
 @section('content')
-    {{$settings}}
-    Settings
+    <h1>Global Site Settings</h1>
+    <hr>
+    @foreach($globalSetting as $setting)
+        <div class="setting" id="{{$setting->id}}" data-type="{{$setting->type}}">
+            <label for="{{$setting->slug}}">{{$setting->name}}</label>
+            @if($setting->type === "checkbox")
+                @if($setting->adminSetting->value)
+                    <input type="checkbox" name="{{$setting->slug}}" id="{{$setting->slug}}" checked="checked" />
+                @else
+                    <input type="checkbox" name="{{$setting->slug}}" id="{{$setting->slug}}" />
+                @endif
+            @elseif($setting->type === "textbox")
+                <input type="text" class="form-control" name="{{$setting->slug}}" id="{{$setting->slug}}" value="{{$setting->adminSetting->value}}" />
+            @elseif($setting->type === "number")
+                <input type="number" class="form-control" name="{{$setting->slug}}" id="{{$setting->slug}}" value="{{$setting->adminSetting->value}}" />
+            @elseif($setting->type === "email")
+                <input type="email" class="form-control" name="{{$setting->slug}}" id="{{$setting->slug}}" value="{{$setting->adminSetting->value}}" />
+            @endif
+        </div>
+    @endforeach
+    <br class="clear">
+    <h1>Your Settings</h1>
+    <hr>
 @stop
