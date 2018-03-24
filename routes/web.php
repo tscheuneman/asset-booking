95703290e330b05c1fd8f2324f9ac62ab3243161
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-23 17:03:42 -0700

    Create global settings update route

--- routes/web.php
+++ routes/web.php
@@ -116,8 +116,9 @@ Route::group(['middleware' => ['cas.admin']], function ()
     Route::post('/admin/asset/specifications/{id}', 'Admin\SpecificationController@show');
     Route::get('/admin/location/verify', 'Admin\LocationController@verify');
 
-
     Route::get('/admin/settings', 'Admin\SettingController@index');
+
+    Route::post('/admin/settings/global', 'Admin\SettingController@globalUpdate');
 });
 
 
