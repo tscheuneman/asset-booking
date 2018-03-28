e1df98c69fcedcfd781a6230714e7a5ed79b206a
Thomas Scheuneman <thomas@tswebvisions.com>
2018-03-28 15:27:26 -0700

    Create approval controller
    Create logic for displaying all unaproved users.
    
    Also create logic for approving a user

---
+++ app/Http/Controllers/Admin/UserApprovalController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminBaseController;
+use App\User;
+
+
+class UserApprovalController extends AdminBaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::where('active', '=', false)->paginate(config('globalSettings.entries-per-page'));
+        return view('admin.user_approval.userapproval',
+            [
+                'users' => $users
+            ]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id)->first();
+
+        if($user != null) {
+            $user->active = true;
+            $user->save();
+            \Session::flash('flash_created',$user->first_name . ' account has been approved');
+            return redirect('/admin/user/approval');
+        }
+        else {
+            \Session::flash('flash_deleted','Failed to approve account');
+            return redirect('/admin/user/approval');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
