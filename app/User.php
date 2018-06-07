fd902073c08ec409fadea5f5ba5e246f127504eb
Thomas Scheuneman <thomas@tswebvisions.com>
2018-06-07 16:25:10 -0700

    Create user model relation

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function departments() {
+        return $this->hasMany('App\UserDepartment', 'user_id');
+    }
+
+
 }
